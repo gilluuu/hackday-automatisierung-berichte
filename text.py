256d3af5646876c84a87d094f55c06537691f711
DESKTOP-16C8CU5\bjoer <langb4@bfh.ch>
2023-05-12 18:31:00 +0200

    added getBericht2()

--- text.py
+++ text.py
@@ -2,16 +2,21 @@ from getBEData import getChart1
 
 df = getChart1()
 
+print(df)
+
 
 data = df.to_dict(orient='index')
+#print(data)
+
 highestyear = max(data.keys())
 thisyear = data[highestyear]
-lastyear = data[2021]
+lastyear = data[highestyear-1]
 
 # Berichtsvariablen
 
 gesamtindex = thisyear['Total']
 änderung_vorjahr = thisyear['Total'] - lastyear['Total']
+gesamtänderung = (thisyear['Total'] - 1) * 100
 
 
 # Logische Anpassung des Textes
@@ -32,16 +37,36 @@ for key, value in thisyear.items():
         elif value < gesamtindex:
             zimmer_unter.append(key)
 
-gesamtindex_formated = "{:.2f}".format(gesamtindex)
+gesamtindex_str = "{:.2f}".format(gesamtindex)
 zimmer_über_str = ', '.join(str(i) for i in zimmer_über)
 zimmer_unter_str = ', '.join(str(i) for i in zimmer_unter)
 
 def getBericht1():
 # Bericht generieren
-    bericht = f"Der Berner Index der Wohnungsmietpreise erreicht im November {highestyear} einen Stand von {gesamtindex_formated} Punkten "
+    bericht = f"Der Berner Index der Wohnungsmietpreise erreicht im November {highestyear} einen Stand von {gesamtindex_str} Punkten "
     bericht += f"(Basis: November 2003 = 100), was {zunahme_text}gegenüber dem Vorjahr entspricht. "
     bericht += f"Die Preisentwicklung der {zimmer_über_str}-Wohungen lag über dem Gesamtindex. "
     bericht += f"Bei den {zimmer_unter_str}-Wohungen lag sie darunter."
     return bericht
 
-print(getBericht1())
+#print(getBericht1())
+
+
+gesamtänderung_str = "{:.2f}".format(gesamtänderung)
+hihgestwohunung = max(thisyear, key=thisyear.get)
+hihgestwohunung_change = "{:.2f}".format((thisyear[hihgestwohunung] - 1) *100)
+
+lowestwohunung = min(thisyear, key=thisyear.get)
+lowesttwohunung_change = "{:.2f}".format((thisyear[lowestwohunung] - 1) *100)
+
+
+def getBericht2():
+# Bericht generieren
+    bericht = f"Seit der Basislegung November 2003 = 100, also im Zeitraum der letzten {highestyear - 2003} Jahre, "
+    bericht += f"stiegen die Wohnungsmietpreise in der Stadt Bern um insgesamt {gesamtänderung_str}%. Während die {hihgestwohunung}wohnungen" 
+    bericht += f"die stärkste Verteuerung erfuhren ({hihgestwohunung_change}), wurde bei den {lowestwohunung}wohnungen"
+    bericht += f"der geringste Anstiegverzeichnet ({lowesttwohunung_change}%)"
+    return bericht
+
+print(getBericht2())
+#print(thisyear)
