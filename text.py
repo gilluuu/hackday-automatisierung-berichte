dcf03509a6d0e9efd513ef6fa777a58b6373e119
abishanaru <aruma2@bfh.ch>
2023-05-12 15:08:01 +0200

    weitere Sätze hinzugefügt

--- text.py
+++ text.py
@@ -1,26 +1,38 @@
-def create_sentence(index, vorjahresindex):
+# Berichtsvariablen
+monat = "November"
+jahr = 2022
+basis_jahr = 2003
+gesamtindex = 124.3
+zunahme_vorjahr = 0.0  # Keine Veränderung
+zimmer_indices = {
+    "1-Zimmerwohnungen": 120.5,
+    "2-Zimmerwohnungen": 130.2,
+    "3-Zimmerwohnungen": 135.8,
+    "4-Zimmerwohnungen": 140.1,
+    "5-Zimmerwohnungen": 142.7
+}
 
-    print('index:',index,'vorjahr index', vorjahresindex )
+# Logische Anpassung des Textes
+zunahme_text = ""
+if zunahme_vorjahr > 0:
+    zunahme_text = f"einer Zunahme um {abs(zunahme_vorjahr)}% gegenüber dem Vorjahr"
+elif zunahme_vorjahr < 0:
+    zunahme_text = f"einer Abnahme um {abs(zunahme_vorjahr)}% gegenüber dem Vorjahr"
+else:
+    zunahme_text = "keiner Veränderung gegenüber dem Vorjahr"
 
-    prozentsatz = "{:.2f}%".format(abs((index - vorjahresindex)/vorjahresindex * 100))
+# Preisentwicklungen
+preisentwicklung_text = ""
+if all(zimmer_indices[zimmer] > gesamtindex for zimmer in zimmer_indices):
+    preisentwicklung_text = "lagen über jener des Gesamtindex"
+elif all(zimmer_indices[zimmer] < gesamtindex for zimmer in zimmer_indices):
+    preisentwicklung_text = "lagen unter jener des Gesamtindex"
 
+# Bericht generieren
+bericht = f"Der Berner Index der Wohnungsmietpreise erreicht im {monat} {jahr} einen Stand von {gesamtindex} Punkten "
+bericht += f"(Basis: {basis_jahr} = 100), was {zunahme_text} entspricht. "
+bericht += f"Die Preisentwicklung der 2- bis 5-Zimmerwohnungen {preisentwicklung_text}. "
+bericht += f"Bei den 1-Zimmerwohnungen lag sie darunter."
 
-    if index > vorjahresindex:
-        wort = 'einer Zunahme um ' + prozentsatz
-    elif index < vorjahresindex:
-        wort = 'einer Abnahme um ' + prozentsatz
-    else:
-        wort = 'einem unveränderten index'
-
-
-
-    string = '''Der Berner Index der Wohnungsmietpreise erreicht im November
-                2022 einen Stand von ''' + str(index) + ''' Punkten (Basis: November 2003 = 100),
-                was ''' + wort + ''' gegenüber dem Vorjahr entspricht.'''
-    
-    return string
-
-
-text = create_sentence(9, 10)
-
-print(text)
+# Ausgabe des Berichts
+print(bericht)
