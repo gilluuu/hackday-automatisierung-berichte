69972abd596d2c21f6af6ca5f45a2a4c35edcd47
DESKTOP-16C8CU5\bjoer <langb4@bfh.ch>
2023-05-12 17:09:51 +0200

    added getBericht1()

--- text.py
+++ text.py
@@ -1,38 +1,45 @@
+from getBEData import getChart1
+
+df = getChart1()
+
+
+data = df.to_dict(orient='index')
+thisyear = data[2022]
+lastyear = data[2021]
+
 # Berichtsvariablen
-monat = "November"
+
 jahr = 2022
 basis_jahr = 2003
-gesamtindex = 124.3
-zunahme_vorjahr = 0.0  # Keine Veränderung
-zimmer_indices = {
-    "1-Zimmerwohnungen": 120.5,
-    "2-Zimmerwohnungen": 130.2,
-    "3-Zimmerwohnungen": 135.8,
-    "4-Zimmerwohnungen": 140.1,
-    "5-Zimmerwohnungen": 142.7
-}
+gesamtindex = thisyear['Total']
+änderung_vorjahr = thisyear['Total'] - lastyear['Total']
+
 
 # Logische Anpassung des Textes
 zunahme_text = ""
-if zunahme_vorjahr > 0:
-    zunahme_text = f"einer Zunahme um {abs(zunahme_vorjahr)}% gegenüber dem Vorjahr"
-elif zunahme_vorjahr < 0:
-    zunahme_text = f"einer Abnahme um {abs(zunahme_vorjahr)}% gegenüber dem Vorjahr"
+if änderung_vorjahr > 0:
+    zunahme_text = f"einer Zunahme um {abs(änderung_vorjahr)}% "
+elif änderung_vorjahr < 0:
+    zunahme_text = f"einer Abnahme um {abs(änderung_vorjahr)}% "
 else:
-    zunahme_text = "keiner Veränderung gegenüber dem Vorjahr"
+    zunahme_text = "keiner Veränderung "
 
-# Preisentwicklungen
-preisentwicklung_text = ""
-if all(zimmer_indices[zimmer] > gesamtindex for zimmer in zimmer_indices):
-    preisentwicklung_text = "lagen über jener des Gesamtindex"
-elif all(zimmer_indices[zimmer] < gesamtindex for zimmer in zimmer_indices):
-    preisentwicklung_text = "lagen unter jener des Gesamtindex"
+zimmer_über = []
+zimmer_unter = []
+for key, value in thisyear.items():
+    if key != 'Total':
+        if value > gesamtindex:
+            zimmer_über.append(key)
+        elif value < gesamtindex:
+            zimmer_unter.append(key)
 
+def getBericht1():
 # Bericht generieren
-bericht = f"Der Berner Index der Wohnungsmietpreise erreicht im {monat} {jahr} einen Stand von {gesamtindex} Punkten "
-bericht += f"(Basis: {basis_jahr} = 100), was {zunahme_text} entspricht. "
-bericht += f"Die Preisentwicklung der 2- bis 5-Zimmerwohnungen {preisentwicklung_text}. "
-bericht += f"Bei den 1-Zimmerwohnungen lag sie darunter."
+    bericht = f"Der Berner Index der Wohnungsmietpreise erreicht im November {jahr} einen Stand von {gesamtindex} Punkten "
+    bericht += f"(Basis: {basis_jahr} = 100), was {zunahme_text}gegenüber dem Vorjahr entspricht. "
+    bericht += f"Die Preisentwicklung der {zimmer_über}-Wohungen lag über dem Gesamtindex. "
+    bericht += f"Bei den {zimmer_unter}-Wohungen lag sie darunter."
+    return bericht
 
-# Ausgabe des Berichts
-print(bericht)
+
+print(getBericht1())
