6e8241bb9fa8688f0c049ccfa12ad00c3ff1e2d2
DESKTOP-16C8CU5\bjoer <langb4@bfh.ch>
2023-05-12 17:54:42 +0200

    finalize getBericht1()

--- text.py
+++ text.py
@@ -4,13 +4,12 @@ df = getChart1()
 
 
 data = df.to_dict(orient='index')
-thisyear = data[2022]
+highestyear = max(data.keys())
+thisyear = data[highestyear]
 lastyear = data[2021]
 
 # Berichtsvariablen
 
-jahr = 2022
-basis_jahr = 2003
 gesamtindex = thisyear['Total']
 änderung_vorjahr = thisyear['Total'] - lastyear['Total']
 
@@ -18,9 +17,9 @@ gesamtindex = thisyear['Total']
 # Logische Anpassung des Textes
 zunahme_text = ""
 if änderung_vorjahr > 0:
-    zunahme_text = f"einer Zunahme um {abs(änderung_vorjahr)}% "
+    zunahme_text = f"einer Zunahme um {'{:.2f}'.format(abs(änderung_vorjahr))}% "
 elif änderung_vorjahr < 0:
-    zunahme_text = f"einer Abnahme um {abs(änderung_vorjahr)}% "
+    zunahme_text = f"einer Abnahme um {'{:.2f}'.format(abs(änderung_vorjahr))}% "
 else:
     zunahme_text = "keiner Veränderung "
 
@@ -33,13 +32,16 @@ for key, value in thisyear.items():
         elif value < gesamtindex:
             zimmer_unter.append(key)
 
+gesamtindex_formated = "{:.2f}".format(gesamtindex)
+zimmer_über_str = ', '.join(str(i) for i in zimmer_über)
+zimmer_unter_str = ', '.join(str(i) for i in zimmer_unter)
+
 def getBericht1():
 # Bericht generieren
-    bericht = f"Der Berner Index der Wohnungsmietpreise erreicht im November {jahr} einen Stand von {gesamtindex} Punkten "
-    bericht += f"(Basis: {basis_jahr} = 100), was {zunahme_text}gegenüber dem Vorjahr entspricht. "
-    bericht += f"Die Preisentwicklung der {zimmer_über}-Wohungen lag über dem Gesamtindex. "
-    bericht += f"Bei den {zimmer_unter}-Wohungen lag sie darunter."
+    bericht = f"Der Berner Index der Wohnungsmietpreise erreicht im November {highestyear} einen Stand von {gesamtindex_formated} Punkten "
+    bericht += f"(Basis: November 2003 = 100), was {zunahme_text}gegenüber dem Vorjahr entspricht. "
+    bericht += f"Die Preisentwicklung der {zimmer_über_str}-Wohungen lag über dem Gesamtindex. "
+    bericht += f"Bei den {zimmer_unter_str}-Wohungen lag sie darunter."
     return bericht
-
 
 print(getBericht1())
